ffd35aadcfe53e820cc1534c6d460d59fc903338
Kevin Jacoby <kvnjcby@gmail.com>
2016-09-26 15:57:40 -0400

    UNC logo

--- wp-content/themes/prospective/templates/header.php
+++ wp-content/themes/prospective/templates/header.php
@@ -1,13 +1,16 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <img src="<?= get_template_directory_uri(); ?>/assets/images/logo.png" width="200">
-    <nav class="nav-primary">
+  <div class="main-header container">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?= get_template_directory_uri(); ?>/assets/images/logo.png" width="275" alt="<?php bloginfo('name'); ?>"></a>
+    
+    <a class="unc" href="http://unc.edu"><img src="<?= get_template_directory_uri(); ?>/assets/images/unc.png" alt="UNC"></a>
+  </div>
+  <nav class="nav-primary">
+    <div class="container">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
       endif;
       ?>
+    </div>
     </nav>
-  </div>
 </header>
