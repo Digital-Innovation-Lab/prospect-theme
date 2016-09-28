79696897f43811ba6d1791b9b89b60512d02b8f8
Kevin Jacoby <kvnjcby@gmail.com>
2016-09-28 15:50:32 -0400

    Homepage slider styles

--- wp-content/themes/prospective/templates/header.php
+++ wp-content/themes/prospective/templates/header.php
@@ -14,3 +14,11 @@
     </div>
     </nav>
 </header>
+
+<?php if (is_front_page()) : ?>
+<div class="slider"><img src="http://localhost:8888/prospect-theme/wp-content/uploads/2016/09/unnamed-700x300.jpg"><aside><h1>Welcome to Prospect</h1>Prospect is a sophisticated web-app implemented as a plugin for WordPress that enables users to collect and curate data and then enable the wider public to visualize and access that data. The graphical representation of data – whether it be geographical information shown on maps, temporal data shown on timelines, interpersonal relationships shown as connected graphs, etc. – can facilitate end-users in comprehending it quickly and analyzing it in domain-specific ways. <a class="more" href="#">Read More</a></aside></div>
+
+<div id="news">
+  <div class="container"><h5>Prospect 1.3 Released</h5></div>
+</div>
+<?php endif; ?>
