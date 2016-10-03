da0a46847a744c1ea65afda0228d4e74003f3470
Kevin Jacoby <kvnjcby@gmail.com>
2016-10-03 14:52:03 -0400

    Styles

--- wp-content/themes/prospective/templates/header.php
+++ wp-content/themes/prospective/templates/header.php
@@ -16,9 +16,16 @@
 </header>
 
 <?php if (is_front_page()) : ?>
-<div class="slider"><img src="http://localhost:8888/prospect-theme/wp-content/uploads/2016/09/unnamed-700x300.jpg"><aside><h1>Welcome to Prospect</h1>Prospect is a sophisticated web-app implemented as a plugin for WordPress that enables users to collect and curate data and then enable the wider public to visualize and access that data. The graphical representation of data – whether it be geographical information shown on maps, temporal data shown on timelines, interpersonal relationships shown as connected graphs, etc. – can facilitate end-users in comprehending it quickly and analyzing it in domain-specific ways. <a class="more" href="#">Read More</a></aside></div>
-
+<div class="slider">
+  <img src="http://localhost:8888/prospect-theme/wp-content/uploads/2016/09/unnamed-700x300.jpg">
+  <aside>
+    <h1>Welcome to Prospect</h1>
+    <p>Prospect is a sophisticated web-app implemented as a plugin for WordPress that enables users to collect and curate data and then enable the wider public to visualize and access that data.</p>
+    <p>The graphical representation of data – whether it be geographical information shown on maps, temporal data shown on timelines, interpersonal relationships shown as connected graphs, etc.</p>
+    <a class="more" href="#">Read More</a>
+  </aside>
+</div>
 <div id="news">
-  <div class="container"><h5>Latest News</h5></div>
+  <div class="container"><h4>Latest News</h4></div>
 </div>
 <?php endif; ?>
